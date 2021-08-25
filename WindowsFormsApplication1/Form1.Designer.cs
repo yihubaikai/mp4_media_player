d08ee02dc7c8e50a96d65f42e0976f79d9d5de4f
yihubaikai <82234032@qq.com>
2021-08-25 13:21:50 +0800

    c# MP4播放器
    c# MP4播放器

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(552, 1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(608, 645);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1067, 652);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "刷新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(118, 48);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(117, 22);
+            this.toolStripMenuItem1.Text = "删除(&D)";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(117, 22);
+            this.toolStripMenuItem2.Text = "清空(&C)";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(679, 663);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(382, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "D:\\code\\2.pycode\\13.douyin\\video";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "连续播放",
+            "单曲循环",
+            "播放一次"});
+            this.comboBox1.Location = new System.Drawing.Point(552, 663);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(83, 20);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "连续播放";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(-5, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(542, 680);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 704);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "简易视频播放器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Collections;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]  public static extern void OutputDebugString(string message);
+
+        static ArrayList al = new ArrayList();
+
+        string Cur_Video_Path = "";
+        int Cur_Index = 0;
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.listView1.Columns.Add("文件路径", 1000, HorizontalAlignment.Left); //一步添加
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FolderBrowserDialog fbd = new FolderBrowserDialog();
+            
+            if (folderBrowserDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            string Cur_dir = "";
+            Cur_dir = folderBrowserDialog1.SelectedPath;
+            this.listView1.Items.Clear();
+            //this.listView1.BeginUpdate();
+            LoadSongList(Cur_dir);
+            //this.listView1.EndUpdate();  //结束数据处理，UI界面一次性绘制
+
+
+        }
+        private bool LoadSongList(string dPath)
+        {
+           
+            DirectoryInfo TheFolder = new DirectoryInfo(dPath);
+            foreach (FileInfo NextFile in TheFolder.GetFiles())
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = dPath + "\\" + NextFile.Name;
+                string d = lvi.Text;
+                d = d.ToLower();
+                if (d.IndexOf(".mp4") == -1 && d.IndexOf(".avi") == -1 && d.IndexOf(".mod") == -1 && d.IndexOf(".wmv") == -1 && d.IndexOf(".mkv") == -1)
+                {
+                    continue;
+                }
+                this.listView1.Items.Add(lvi);
+            }
+            return true;
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //C#如何在ListView失去焦点的情况下仍然保持Item高亮
+            foreach (ListViewItem itm in this.listView1.Items)
+            {
+                itm.BackColor = SystemColors.Window;
+                itm.ForeColor = Color.Black;
+
+            }
+            foreach (ListViewItem itm2 in this.listView1.SelectedItems)
+            {
+                itm2.BackColor = SystemColors.MenuHighlight;
+                itm2.ForeColor = Color.White;
+            }
+
+            ListView.SelectedIndexCollection indexes = listView1.SelectedIndices;
+            // string pr = "";
+             foreach (int index in indexes)
+            {
+                Cur_Video_Path = listView1.Items[index].Text;
+                this.Text = Cur_Video_Path;
+                textBox1.Text = Cur_Video_Path;
+                axWindowsMediaPlayer1.URL = Cur_Video_Path;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                Cur_Index = index;
+                //MessageBox.Show("Line:" + this.listView1.SelectedItems[0]);
+                 break;
+               //pr += listView1.Items[index].Text;
+            }
+            //MessageBox.Show(pr);
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            ListView listView = (ListView)sender;
+            ListViewItem item = listView.GetItemAt(e.X, e.Y);
+            if (item != null && e.Button == MouseButtons.Right)
+            {
+                this.contextMenuStrip1.Show(listView, e.X, e.Y);
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            DateTime beforDT = System.DateTime.Now;
+
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+
+            DateTime afterDT = System.DateTime.Now;
+            TimeSpan ts = afterDT.Subtract(beforDT);  
+            Debug.WriteLine("STOP 总共花费ms."+ts.TotalMilliseconds);  
+
+           
+            //MessageBox.Show("删除:" + this.Cur_Video_Path);
+            string d = this.Cur_Video_Path;
+            al.Add(d);
+            //File.Delete(d);
+
+            afterDT = System.DateTime.Now;
+            ts = afterDT.Subtract(beforDT);  
+            Debug.WriteLine("DEL 总共花费ms."+ ts.TotalMilliseconds);
+
+            //this.listView1.BeginUpdate();
+            listView1.Items.Remove(this.listView1.SelectedItems[0]);
+            afterDT = System.DateTime.Now;
+            ts = afterDT.Subtract(beforDT);  
+            Debug.WriteLine("remove 总共花费{0}ms."+ ts.TotalMilliseconds);
+
+            //this.listView1.EndUpdate();  //结束数据处理，UI界面一次性绘制
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("清空");
+        }
+        
+         private static void ThreadProc(object obj)
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                foreach (string szPath in al)
+                {
+                    if (File.Exists(szPath))
+                    {
+                        Debug.WriteLine(szPath);
+                        File.Delete(szPath);
+                       
+                    }
+                    al.Remove(szPath);
+                    break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //this.listView1.BeginUpdate();
+            string dPath = "D:\\code\\2.pycode\\13.douyin\\video";
+           
+            if (Directory.Exists(dPath) == false)
+            {
+                string Cur_path = System.IO.Directory.GetCurrentDirectory() + "\\video";
+                if (Directory.Exists(Cur_path) == false)
+                {
+                    Directory.CreateDirectory(Cur_path); 
+                }
+                dPath = Cur_path;
+            }
+            folderBrowserDialog1.SelectedPath = dPath;
+
+            //加载目录
+            LoadSongList(dPath);
+
+            //显示加载目录
+            textBox1.Text = dPath;
+            //this.listView1.EndUpdate();  //结束数据处理，UI界面一次性绘制
+
+            Thread t1 = new Thread(ThreadProc);
+            t1.Start(10);            
+        }
+
+        private void listView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 46)
+            {
+                
+                int iIndex = 0;
+                foreach (ListViewItem var in listView1.SelectedItems)
+                {
+                    ListViewItem item = var;
+                    iIndex = var.Index;
+                    
+                }
+                toolStripMenuItem1_Click(sender, null);
+                listView1.Items[iIndex].Selected = true;
+            
+            }
+            //MessageBox.Show("" + e.KeyValue);
+        }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsMediaEnded)
+            {
+                    //自动播放下一首
+                    int iIndex = 0;
+                    foreach (ListViewItem var in listView1.SelectedItems)
+                    {
+                        ListViewItem item = var;
+                        iIndex = var.Index;
+                    }
+                    if (iIndex < listView1.Items.Count) 
+                    {
+                       // this.listView1.BeginUpdate();
+
+                        if (comboBox1.SelectedIndex == 0)
+                        {
+                            Debug.WriteLine(axWindowsMediaPlayer1.URL);
+                            Debug.WriteLine(this.listView1.Items[iIndex + 1].Text);
+                            axWindowsMediaPlayer1.URL = this.listView1.Items[iIndex + 1].Text;
+                            listView1.Items[iIndex].Selected = false;
+                            listView1.Items[iIndex + 1].Selected = true;
+                        }
+                        if (comboBox1.SelectedIndex == 1)
+                        {
+                            Debug.WriteLine(axWindowsMediaPlayer1.URL);
+                            Debug.WriteLine(this.listView1.Items[iIndex + 1].Text);
+                            axWindowsMediaPlayer1.URL = this.listView1.Items[iIndex ].Text;
+                            listView1.Items[iIndex].Selected = false;
+                            listView1.Items[iIndex].Selected = true;
+                        }
+
+                        
+                        
+                        //this.listView1.EndUpdate();
+                    }
+                    
+            }
+            else if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsReady)     //播放器准备就绪后播放
+            {
+                if (comboBox1.SelectedIndex == 0 || comboBox1.SelectedIndex == 1)//自动播放下一首
+                {
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                }
+               
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            if (Cur_Index < listView1.Items.Count)
+            {
+                listView1.Items[Cur_Index].Selected = true;
+                Debug.WriteLine("当前焦点:" + Cur_Index);
+            }
+        }
+    }
+}
